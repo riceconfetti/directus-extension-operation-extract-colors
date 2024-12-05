ccdb10e5c36583bd50e995699d60bc5f8eb330f9
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-12-04 20:17:54 -0500

    pwease

--- src/api.js
+++ src/api.js
@@ -74,9 +74,10 @@ export default defineOperationApi({
         width: 200,
         height: 200,
       };
-      return await extractColors(imageData, imgoptions).sort(
-        (a, b) => b.area - a.area
-      );
+      let colors = await extractColors(imageData, imgoptions);
+      colors.sort((a, b) => b.area - a.area);
+
+      return colors;
     };
 
     return { data: await request(src) };
